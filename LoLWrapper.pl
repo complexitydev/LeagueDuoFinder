88695794372401607553949bd41025323bb36894
Ben Humphrey <bennyh6813@gmail.com>
2017-06-07 07:09:43 -0400

    initial commit

---
+++ LoLWrapper.pl
@@ -0,0 +1,63 @@
+#Ben Humphrey
+#github.com/complexitydevelopment
+#ben@complexitydevelopment.com
+use Cwd qw( abs_path );
+use File::Basename qw ( dirname );
+require("./op.pl");
+
+sub read_config {
+    my $region;
+    my $summoner_name;
+    my $fn = 'config.txt';
+
+    #add update
+    if ( !-e $fn ) {
+        open my $fc, ">", $fn;
+        print "Looks like you don't have a config, let's make one now\n";
+        print "Enter your region, ex : NA, EUW\n";
+        $region = <STDIN>;
+        chomp $region;
+        print "Region set to -> $region\n";
+
+        print "Enter your Summoner Name\n";
+        $summoner_name = <STDIN>;
+        chomp $summoner_name;
+        print "Summoner name set to $summoner_name\n";
+
+        my $cd = dirname( abs_path($0) );
+        print "Saving to config in current directory $cd\n";
+
+        print $fc "$region\n";
+        print $fc "$summoner_name\n";
+        close $fc;
+    }
+    else {
+        open my $fh, '<', $fn
+          or die "Failed to open file, is it currently open?";
+        print "Reading Config\n";
+        chomp( my @config = <$fh> );
+        $region        = $config[0];
+        $summoner_name = $config[1];
+        print "Found -> Region: $region , Summoner Name : $summoner_name\n";
+        close $fh;
+    }
+    return ( \$region, \$summoner_name );
+}
+
+sub init_prog {
+    my ( $region, $summoner_name ) = read_config();
+    init_crawler( $region, $summoner_name );
+    my $content = get_userpage();
+    my $fn      = 'debugpage.txt';
+    if ( !-e $fn ) {
+        open my $fh, ">", $fn;
+        print $fh $content;
+    }
+    my $hash_ref = enumerate_summoners();
+    foreach my $key ( keys %$hash_ref ) {
+        print "Key: $key , Value : $hash_ref->{$key}\n";
+    }
+    process_userpages($hash_ref);
+}
+
+init_prog();
